441cbf95886cbb99d615a151a1f3c9029b410f02
Anastasija3587 <Anastasija3587gmail.com>
2019-12-06 00:25:58 +0200

    Updates

---
+++ precache-manifest.dd6e1e451d675cae8594c7f92fde1fc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc471475d8b4a1d828ba1e865017186d",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "b9361b5b5d699a4050f0",
+    "url": "/goit-react-hw-01-components/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "0c826c01f2f3f8cf4dbe",
+    "url": "/goit-react-hw-01-components/static/css/main.2dc7c0db.chunk.css"
+  },
+  {
+    "revision": "b9361b5b5d699a4050f0",
+    "url": "/goit-react-hw-01-components/static/js/2.65bd1d9c.chunk.js"
+  },
+  {
+    "revision": "0c826c01f2f3f8cf4dbe",
+    "url": "/goit-react-hw-01-components/static/js/main.31ae41e7.chunk.js"
+  },
+  {
+    "revision": "c4036953cb714bd39313",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.02e29828.js"
+  }
+]);
